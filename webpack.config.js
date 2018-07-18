8bf5c18ea85e0a8ee5205dd18a2dee7309a1bb6c
Graham Neilson <graham.c.neilson@gmail.com>
2018-07-18 21:38:50 +0100

    chore, replace double quotes

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 
