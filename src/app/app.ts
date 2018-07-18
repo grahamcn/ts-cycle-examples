55e30201bbe16988bd4867f8d31e23d97e071c59
Graham Neilson <graham.c.neilson@gmail.com>
2018-07-18 22:26:24 +0100

    chore, tidy the counter

--- src/app/app.ts
+++ src/app/app.ts
@@ -13,9 +13,17 @@ function mapNumberToMessage(i: number): VNode {
 	)
 }
 
+function add(n) {
+	return function (i: number): number {
+		return i + n
+	}
+}
+
 function App(): Sinks {
 	const vdom$ =
 		xs.periodic(1000)
+			.startWith(-1)
+			.map(add(1))
 			.map(mapNumberToMessage)
 
 	return {
