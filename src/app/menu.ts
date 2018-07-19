373780118e0abefce26943056aeed3b506f15955
Graham Neilson <graham.c.neilson@gmail.com>
2018-07-19 18:10:29 +0100

    chore, package-lock changes post install

--- src/app/menu.ts
+++ src/app/menu.ts
@@ -6,52 +6,52 @@ import { RequestInput, HTTPSource } from '@cycle/http'
 import { dropRepeats } from './misc/xstream.extra'
 
 import {
-  pick,
-  transformPathToSecondaryDataKey,
-  getTertiaryMenuDataUrl
+	pick,
+	transformPathToSecondaryDataKey,
+	getTertiaryMenuDataUrl
 } from './misc/helpers'
 
 interface Sinks {
-  DOM: Stream<VNode>,
-  HTTP: Stream<RequestInput>
+	DOM: Stream<VNode>,
+	HTTP: Stream<RequestInput>
 }
 
 interface Sources {
-  History: Stream<Location>,
-  HTTP: HTTPSource,
+	History: Stream<Location>,
+	HTTP: HTTPSource,
 }
 
 function Menu(sources: Sources): Sinks {
 
-  // define a stream of sport
-  const secondaryDataKey$ =
-    sources.History
-      .map(pick('pathname'))
-      .map(transformPathToSecondaryDataKey)
-      .compose(dropRepeats()) // akin to memoize / shouldComponentUpdate. if we change urls, we don't change menu unless segment 1 changes
+	// define a stream of sport
+	const secondaryDataKey$ =
+		sources.History
+			.map(pick('pathname'))
+			.map(transformPathToSecondaryDataKey)
+			.compose(dropRepeats()) // akin to memoize / shouldComponentUpdate. if we change urls, we don't change menu unless segment 1 changes
 
-  // define a stream of menu data requests
-  const menuHttp$ =
-    secondaryDataKey$.map(key => ({
-      url: getTertiaryMenuDataUrl(key),
-      'category': 'tertiary-menu'
-    }))
+	// define a stream of menu data requests
+	const menuHttp$ =
+		secondaryDataKey$.map(key => ({
+			url: getTertiaryMenuDataUrl(key),
+			'category': 'tertiary-menu'
+		}))
 
-  // define a stream of menu data responses
-  const menuResponse$ =
-    sources.HTTP
-      .select('tertiary-menu')
-      .flatten()
+	// define a stream of menu data responses
+	const menuResponse$ =
+		sources.HTTP
+			.select('tertiary-menu')
+			.flatten()
 
-  const vdom$ =
-    menuResponse$
-      .map(res => div(JSON.stringify(res)))
-      .startWith(div('loading...'))
+	const vdom$ =
+		menuResponse$
+			.map(res => div(JSON.stringify(res)))
+			.startWith(div('loading...'))
 
-  return {
-    DOM: vdom$,
-    HTTP: menuHttp$,
-  }
+	return {
+		DOM: vdom$,
+		HTTP: menuHttp$,
+	}
 }
 
 export default Menu
