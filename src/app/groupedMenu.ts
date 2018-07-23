c2ba665d899cab229d90fe94ff90378066e412df
Graham Neilson <graham.neilson@skybettingandgaming.com>
2018-07-23 12:24:21 +0100

    rename menu group groupedMenu - inline with simple menu name pattern - include file

---
+++ src/app/groupedMenu.ts
@@ -0,0 +1,33 @@
+import { VNode, DOMSource, div, li } from '@cycle/dom'
+import { Stream } from 'xstream'
+import { StateSource } from 'cycle-onionify'
+
+import { Menu } from './sideMenu'
+
+interface State extends Menu {}
+
+interface Sinks {
+	DOM: Stream<VNode>,
+}
+
+interface Sources {
+	DOM: DOMSource,
+	onion: StateSource<State>
+}
+
+function GroupedMenu(sources: Sources): Sinks {
+	const state$ = sources.onion.state$
+
+	const vdom$ =
+    state$.map(({title}) =>
+      li('.list',
+        div('menu group - ' + title)
+      )
+		)
+
+	return {
+		DOM: vdom$,
+	}
+}
+
+export default GroupedMenu
