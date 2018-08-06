402ea79bca0a8f01e74055e1cb48bdf0a94480ac
Graham Neilson <graham.c.neilson@gmail.com>
2018-08-06 19:20:43 +0100

    simple carousel

--- src/app/carousel.ts
+++ src/app/carousel.ts
@@ -1,5 +1,6 @@
 import xs, { Stream } from 'xstream'
 import { div, VNode, h2, nav, button, ul, li, DOMSource,  } from '@cycle/dom'
+import { request } from 'http';
 
 interface Sinks {
 	DOM: Stream<VNode>,
@@ -17,23 +18,56 @@ const slidesData = [
 	'marzipan',
 ]
 
+// this could be a good example to refactor to MVI
+
 function Carousel(sources: Sources): Sinks {
 
-	// reduce to a +/- someting from previos value
-	const selectedIndex$: Stream<string> =
+	const slideIndexOffset$: Stream<number> =
 		sources.DOM
 			.select('.goToslide')
 			.events('click')
 			.map(event => {
 				const target: EventTarget = event.target
-				return target['dataset'].dataSlideIndex
-			}).startWith('0')
+				return parseInt(target['dataset'].dataSlideOffset)
+			})
 
-	// timer gives +1
-	// next gives +1
-	// prev gives -1
-	// selected gives +/- some value
-	// merge that to reducee, start with 0
+	const nextOffset$ = sources.DOM.select('.next').events('click').mapTo(1)
+	const prevOffset$ = sources.DOM.select('.prev').events('click').mapTo(-1)
+
+	// any click on those will reset the timer, so let's create that stream of clicks.
+	const carouselControlClick$: Stream<any> =
+		xs.merge(
+			slideIndexOffset$,
+			prevOffset$,
+			nextOffset$,
+		)
+
+	const timerOffset$: Stream<number> =
+		carouselControlClick$
+			.startWith(0)
+			.map(() =>
+				xs.periodic(2000)
+			)
+			.flatten()
+			.mapTo(1)
+
+	const selectedIndex$ =
+		xs.merge(
+			timerOffset$,
+			slideIndexOffset$,
+			nextOffset$,
+			prevOffset$,
+		).fold((acc, offset) => {
+			const requestedSlide = acc + offset
+
+			if (requestedSlide < 0) {
+				return slidesData.length - 1
+			} else if (requestedSlide > slidesData.length - 1) {
+				return 0
+			}
+
+			return requestedSlide
+		}, 0)
 
 	const vdom$: Stream<VNode> =
 		selectedIndex$
@@ -41,7 +75,7 @@ function Carousel(sources: Sources): Sinks {
 				div('.carousel', [
 					...slidesData
 						.map((slide, index) =>
-							index.toString() !== selectedIndex ? undefined :
+							index !== selectedIndex ? undefined :
 								div(`.slide .slide-${index}`, {
 									style: {
 										backgroundColor: 'grey',
@@ -51,7 +85,7 @@ function Carousel(sources: Sources): Sinks {
 										position: 'absolute',
 										width: '100%',
 										height: '200px',
-										transition: 'opacity 500ms',
+										transition: 'opacity 500ms', // animation stuff starts here
 										opacity: 0.01,
 										delayed: {
 											opacity: 1,
@@ -67,9 +101,12 @@ function Carousel(sources: Sources): Sinks {
 						ul(
 							slidesData.map((slide, index) =>
 								li(
-									button('.goToslide', {
+									// I've had to change the class too as the dataset change does not
+									// get picked up / cause a re-render. that's quite interesting, and
+									// potentially useful to know
+									button(`.goToslide offset-${selectedIndex - index}`, {
 										dataset: {
-											dataSlideIndex: index.toString()
+											dataSlideOffset: (selectedIndex - index) * -1
 										}
 									}, index)
 								)
